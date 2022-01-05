a60d251c97e45b8c3985b51fbc2b7be7100e0b56
CAPCO\KYEH <karim.yehia@capco.com>
2022-01-05 16:50:56 -0300

    initial commit

---
+++ hr-oauth/src/main/java/com/devsuperior/oath/resource/UserResource.java
@@ -0,0 +1,31 @@
+package com.devsuperior.oath.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.oath.entities.User;
+import com.devsuperior.oath.services.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+	
+	@Autowired
+	private UserService service;
+	
+	@GetMapping(value="/search")
+	public ResponseEntity<User> findById(@RequestParam String email){
+		try {
+		User user = service.findByEmail(email);
+		return ResponseEntity.ok(user);
+		}catch (IllegalArgumentException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+
+}
---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.hrpayroll.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
+
+@Service //Duvida
+public class PaymentService {
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
+	public Payment getPayment(long workId, int days) {
+	
+		Worker worker = workerFeignClient.findById(workId).getBody();
+		return new Payment(worker.getName(),worker.getDailyIncome(),days);
+	}
+	
+
+}
